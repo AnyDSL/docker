6605cd07cfbb375740bdf1f9c5974c2b2cddbb3a
Roland Leißa <leissa@cs.uni-saarland.de>
2018-06-30 02:19:43 +0200

    switching to arch linux
    Alpine's libc choice is causing problems that Arch doesn't have.

--- build.sh
+++ build.sh
@@ -1,14 +1,17 @@
 #!/bin/bash
+set -eu
 
 BUILD_TYPE=Release
 CMAKE_MAKE_FLAGS=-j8
 
-docker pull anydsl/build-base
+#docker pull anydsl/build-base
+docker build -t anydsl/build-base build-base
 docker build -t anydsl/llvm:5.0.2-${BUILD_TYPE} --build-arg CMAKE_BUILD_TYPE=${BUILD_TYPE} --build-arg CMAKE_MAKE_FLAGS=${CMAKE_MAKE_FLAGS} llvm
 docker build -t anydsl/thorin:master-${BUILD_TYPE} --build-arg CMAKE_BUILD_TYPE=${BUILD_TYPE} --build-arg CMAKE_MAKE_FLAGS=${CMAKE_MAKE_FLAGS} thorin
 docker build -t anydsl/impala:master-${BUILD_TYPE} --build-arg CMAKE_BUILD_TYPE=${BUILD_TYPE} --build-arg CMAKE_MAKE_FLAGS=${CMAKE_MAKE_FLAGS} impala
 docker build -t anydsl/runtime:master-${BUILD_TYPE} --build-arg CMAKE_BUILD_TYPE=${BUILD_TYPE} --build-arg CMAKE_MAKE_FLAGS=${CMAKE_MAKE_FLAGS} runtime
 
+docker push anydsl/build-base
 docker push anydsl/llvm:5.0.2-${BUILD_TYPE}
 docker push anydsl/thorin:master-${BUILD_TYPE}
 docker push anydsl/impala:master-${BUILD_TYPE}
