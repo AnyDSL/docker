096ebf223ab724b8a2224def450abd42dca28ef8
Roland Leißa <leissa@cs.uni-saarland.de>
2018-07-02 18:40:47 +0200

    typo

--- build.sh
+++ build.sh
@@ -5,10 +5,10 @@ CMAKE_MAKE_FLAGS=-j8
 
 #docker pull anydsl/build-base
 docker build -t anydsl/build-base build-base
-docker build -t anydsl/llvm   --build-arg CMAKE_MAKE_FLAGS=${CMAKE_MAKE_FLAGS} llvm
-docker build -t anydsl/thorin --build-arg CMAKE_MAKE_FLAGS=${CMAKE_MAKE_FLAGS} thorin
-docker build -t anydsl/impala --build-arg CMAKE_MAKE_FLAGS=${CMAKE_MAKE_FLAGS} impala
-docker build -t anydsl/runtim --build-arg CMAKE_MAKE_FLAGS=${CMAKE_MAKE_FLAGS} runtime
+docker build -t anydsl/llvm    --build-arg CMAKE_MAKE_FLAGS=${CMAKE_MAKE_FLAGS} llvm
+docker build -t anydsl/thorin  --build-arg CMAKE_MAKE_FLAGS=${CMAKE_MAKE_FLAGS} thorin
+docker build -t anydsl/impala  --build-arg CMAKE_MAKE_FLAGS=${CMAKE_MAKE_FLAGS} impala
+docker build -t anydsl/runtime --build-arg CMAKE_MAKE_FLAGS=${CMAKE_MAKE_FLAGS} runtime
 
 docker push anydsl/build-base
 docker push anydsl/llvm
